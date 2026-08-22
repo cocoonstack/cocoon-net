f876e1765ec61cbd68e02a7bf4248835ef014c0b
czmDeRepository <56431414+czmDeRepository@users.noreply.github.com>
2026-08-22 16:40:54 +0800

    test: cover secondary NIC startup recovery
    Change-Id: I6149490e5e98dbf713a7a1afc64d651a017312a8

--- node/links_linux.go
+++ node/links_linux.go
@@ -10,15 +10,19 @@ import (
 )
 
 func setupSecondaryNICs(ifaces []string) error {
+	return setupSecondaryNICsWith(ifaces, netlink.LinkByName, netlink.LinkSetUp)
+}
+
+func setupSecondaryNICsWith(ifaces []string, lookup func(string) (netlink.Link, error), setUp func(netlink.Link) error) error {
 	for _, iface := range ifaces {
-		link, err := netlink.LinkByName(iface)
+		link, err := lookup(iface)
 		if err != nil {
 			return fmt.Errorf("lookup link %s: %w", iface, err)
 		}
 		if link.Attrs().Flags&net.FlagUp != 0 {
 			continue
 		}
-		if err := netlink.LinkSetUp(link); err != nil {
+		if err := setUp(link); err != nil {
 			return fmt.Errorf("set link %s up: %w", iface, err)
 		}
 	}
